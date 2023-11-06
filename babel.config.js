6e9a7c6860a2364c87455eca7501c8a08dab1429
tab <tukhfatullin@gmail.com>
2023-11-06 20:33:19 +0200

    Add react-native-reanimated package

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
   plugins: [
     "@realm/babel-plugin",
+    "react-native-reanimated/plugin",
     [
       "module-resolver",
       {
